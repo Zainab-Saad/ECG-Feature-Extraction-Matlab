4c658c1f1aced55b4dae583c101afcde63f99470
Taimoor Hassan <24260004@lums.edu.pk>
2025-01-09 18:13:03 +0500

    latest

--- binary_class_svm.m
+++ binary_class_svm.m
@@ -4,8 +4,9 @@ display('Reading samples ECG signal from MIT-BIH Arrhythmia Database')
 display('Reading and plotting annotations (human labels) of QRS complexes performend on the signals')
 [ann,type,subtype,chan,num]=rdann('mitdbase/207','atr',1);
 
+[R_peaks_val, R_peaks_ind,Q_peaks_ind, Q_peaks_val,S_peaks_ind, S_peaks_val,T_peaks_ind, T_peaks_val, delay] = pan_tompkin(ecg, Fs);
 
-
+%%
 my_classes = ['N' 'V'];
 count = 1;
 V_start = [];
@@ -18,4 +19,5 @@ while count<length(type)
     if type(count) == ']'
         V_stop = [V_stop count];
     end
-    c
+    count = count +1;
+end
